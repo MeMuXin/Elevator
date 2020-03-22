06bccb2b6b24e102c240515f6a7f4f4e42a83ba4
Emmanuel <61671661+MeMuXin@users.noreply.github.com>
2020-03-21 22:54:51 -0300

    Fixed
    Now it checks that the block right below you is wool

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ function Initialize(a_Plugin)
 	a_Plugin:SetName("Elevator");
 	a_Plugin:SetVersion(1);
 	cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_MOVING, JugadorSalta);
-	cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_CROUCHING, JugadorAgacha);
+	cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_CROUCHED, JugadorAgacha);
 
 
 	-- Load the InfoReg shared library:
@@ -26,34 +26,32 @@ end
 function JugadorSalta(Player, OldPosition, NewPosition, PreviousIsOnGround) -- player jumps
 		a_Delta = OldPosition - NewPosition;
 		if (a_Delta.y < 0) then
-			if PreviousIsOnGround == true then
-			--Player:SendMessage(Player:GetWorld():GetBlock(OldPosition.x, OldPosition.y-1, OldPosition.z) .. " META " .. Player:GetWorld():GetBlockMeta(OldPosition.x, OldPosition.y-1, OldPosition.z) .. " META " .. Player:GetWorld():GetHeight(OldPosition.x, OldPosition.z))
-			--revisar desde donde estoy yo hasta el proximo bloque (hacia arriba) numero 35 meta 128 (Lana)
-			for i = OldPosition.y, Player:GetWorld():GetHeight(OldPosition.x, OldPosition.z), 1 do
-				if Player:GetWorld():GetBlock(OldPosition.x, i, OldPosition.z)	== 35 then
-					Player:TeleportToCoords(OldPosition.x, i+1, OldPosition.z)
-					return true
+			if Player:GetWorld():GetBlock(OldPosition.x, OldPosition.y-1, OldPosition.z) == E_BLOCK_WOOL then
+				if PreviousIsOnGround == true then
+					for i = OldPosition.y, Player:GetWorld():GetHeight(OldPosition.x, OldPosition.z), 1 do
+						if Player:GetWorld():GetBlock(OldPosition.x, i, OldPosition.z) == E_BLOCK_WOOL then
+							Player:TeleportToCoords(OldPosition.x, i+1, OldPosition.z)
+							return true
+						end
+					end
 				end
-			end
 			end
 		end
 	return false
 end
 
-function JugadorAgacha(Player, IsCrouched) -- player crouches
-	if IsCrouched then
-			OldPosition = Player:GetPosition()
-			--revisar desde donde estoy yo hasta el proximo (Hacia abajo) bloque numero 35 meta 128 (Lana)
-			local i = OldPosition.y-2
-			while i >= 3 do
-				if Player:GetWorld():GetBlock(OldPosition.x, i, OldPosition.z)	== 35 then
-					Player:TeleportToCoords(OldPosition.x, i+1, OldPosition.z)
-					return true
-				end
-				i = i - 1
+function JugadorAgacha(Player) -- player crouches
+OldPosition = Player:GetPosition()
+	if Player:GetWorld():GetBlock(OldPosition.x, OldPosition.y-1, OldPosition.z) == E_BLOCK_WOOL then
+	--revisar desde donde estoy yo hasta el proximo (Hacia abajo) bloque numero 35 meta 128 (Lana)
+		local i = OldPosition.y-2
+		while i >= 3 do
+			if Player:GetWorld():GetBlock(OldPosition.x, i, OldPosition.z)	== E_BLOCK_WOOL then
+				Player:TeleportToCoords(OldPosition.x, i+1, OldPosition.z)
+				return true
 			end
-	else
-		return false
+			i = i - 1
+		end
 	end
 end
 
